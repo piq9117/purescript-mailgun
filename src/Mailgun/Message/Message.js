e466918c8806a23536ff38c7d999be693f2de73b
Ken Aguilar <piq9117@gmail.com>
2019-01-25 14:52:00 -0600

    Message
    removed extract nested function.

--- src/Mailgun/Message/Message.js
+++ src/Mailgun/Message/Message.js
@@ -1,11 +1,7 @@
 exports.sendMessageImpl = function(messages, msgData, cb) {
-  return function () {
-    return messages.send(msgData, cb);
-  };
+  return messages.send(msgData, cb);
 };
 
 exports.messages = function (mailgun) {
-  return function () {
-    return mailgun.messages();
-  };
+  return mailgun.messages();
 };
