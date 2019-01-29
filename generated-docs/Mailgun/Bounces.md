886db3112f9fc403834a6f76426e6dad39b7cc34
Ken Aguilar <piq9117@gmail.com>
2019-01-28 23:22:13 -0600

    added documentation for actions.

--- generated-docs/Mailgun/Bounces.md
+++ generated-docs/Mailgun/Bounces.md
@@ -21,11 +21,15 @@ type BouncesAttr = { address :: String, code :: Int, error :: String, created_at
 bounces :: Mailgun -> Maybe String -> Bounces
 ```
 
+bounces api.
+
 #### `list`
 
 ``` purescript
 list :: forall a. Bounces -> Callback a -> Effect Unit
 ```
+
+Fetches the list of bounces.
 
 #### `info`
 
@@ -33,16 +37,23 @@ list :: forall a. Bounces -> Callback a -> Effect Unit
 info :: forall a. Bounces -> Callback a -> Effect Unit
 ```
 
+Fetches a single bounces event by a given email address.
+
 #### `delete`
 
 ``` purescript
 delete :: forall a. Bounces -> Callback a -> Effect Unit
 ```
 
+Clears a given bounce event.
+
 #### `create`
 
 ``` purescript
 create :: forall a. Bounces -> BouncesAttr -> Callback a -> Effect Unit
 ```
+
+Adds a permanent bounce to the bounces table. Updates the existing record
+if alread here.
 
 
--- src/Mailgun/Bounces/Bounces.purs
+++ src/Mailgun/Bounces/Bounces.purs
@@ -36,17 +36,23 @@ type BouncesAttr =
   , created_at :: Effect Date
   }
 
+-- | bounces api.
 bounces :: Mailgun -> Maybe String -> Bounces
 bounces mg str = runFn2 bouncesImpl mg (toNullable str)
 
+-- | Fetches the list of bounces.
 list :: ∀ a. Bounces -> Callback a -> Effect Unit
 list b cb = runEffectFn2 listImpl b (handleCallback cb)
 
+-- | Fetches a single bounces event by a given email address.
 info :: ∀ a. Bounces -> Callback a -> Effect Unit
 info b cb = runEffectFn2 infoImpl b (handleCallback cb)
 
+-- | Clears a given bounce event.
 delete :: ∀ a. Bounces -> Callback a -> Effect Unit
 delete b cb = runEffectFn2 deleteImpl b (handleCallback cb)
 
+-- | Adds a permanent bounce to the bounces table. Updates the existing record
+-- | if alread here.
 create :: ∀ a. Bounces -> BouncesAttr -> Callback a -> Effect Unit
 create b attr cb = runEffectFn3 createImpl b attr (handleCallback cb)
