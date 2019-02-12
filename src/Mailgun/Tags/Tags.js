66dc41f09f16b92e43e40185a301cdff99f1302e
Ken Aguilar <piq9117@gmail.com>
2019-02-11 23:20:26 -0600

    Tags
    re-implementing tags.

--- src/Mailgun/Tags/Tags.js
+++ src/Mailgun/Tags/Tags.js
@@ -8,50 +8,6 @@ exports.tagsImpl = function(mailgun, tag) {
     : mailgun.tags(tag);
 };
 
-exports.infoImpl = function(tags, cb) {
-  return tags.info(cb);
-};
-
-exports.aggregatesImpl = function(stats) {
-  return stats.aggregates();
-};
-
-exports.countriesImpl = function(aggregates) {
-  return aggregates.countries();
-};
-
-exports.countriesListImpl = function(countries, cb) {
-  return countries.list(cb);
-};
-
-exports.providersImpl = function(aggregates) {
-  return aggregates.providers();
-};
-
-exports.providersListImpl = function(providers, cb) {
-  return providers.list(cb);
-};
-
-exports.devicesImpl = function(aggregates) {
-  return aggregates.devices();
-};
-
-exports.devicesListImpl = function(devices, cb) {
-  return devices.list(cb);
-};
-
-exports.deleteImpl = function(tags, cb) {
-  return tags.delete(cb);
-};
-
 exports.listImpl = function(tags, cb) {
   return tags.list(cb);
-};
-
-exports.tagStatsImpl = function(tags) {
-  return tags.stats();
-};
-
-exports.tagStatsInfoImpl = function(tagStats, cb) {
-  return tagStats.info(cb);
 };
--- src/Mailgun/Tags/Tags.purs
+++ src/Mailgun/Tags/Tags.purs
@@ -1,112 +1,21 @@
--- | Deletes all counters for particular tag and the tag itself.
-module Mailgun.Tags
-       ( Tags
-       , Aggregates
-       , Countries
-       , Providers
-       , Devices
-       , TagStats
-       , tags
-       , info
-       , list
-       , aggregates
-       , countries
-       , countriesList
-       , providers
-       , providersList
-       , devices
-       , devicesList
-       , delete
-       , tagStats
-       , tagStatsInfo
-       ) where
+module Mailgun.Tags where
 
-import Mailgun.Common
-
-import Data.Function.Uncurried (runFn1, Fn1, Fn2, runFn2)
+import Data.Function.Uncurried (Fn2, runFn2)
 import Data.Maybe (Maybe)
 import Data.Nullable (Nullable, toNullable)
-import Effect (Effect)
-import Effect.Uncurried (EffectFn2, EffectFn3, runEffectFn2)
+import Effect.Uncurried (EffectFn2, runEffectFn2)
 import Mailgun (Mailgun)
+import Mailgun.Common (JSCallback, handleCallback, Callback)
 import Prelude (Unit)
+import Effect (Effect)
 
 foreign import data Tags :: Type
-foreign import data Aggregates :: Type
-foreign import data Countries :: Type
-foreign import data Providers :: Type
-foreign import data Devices :: Type
-foreign import data TagStats :: Type
 
 foreign import tagsImpl :: Fn2 Mailgun (Nullable String) Tags
-foreign import infoImpl :: ∀ a. EffectFn2 Tags (JSCallback a) Unit
-foreign import listImpl :: ∀ a. EffectFn3 Tags TagsAttr (JSCallback a) Unit
-foreign import tagStatsImpl :: Fn1 Tags TagStats
-foreign import tagStatsInfoImpl :: ∀ a. EffectFn2 TagStats (JSCallback a) Unit
-foreign import aggregatesImpl :: Fn1 TagStats Aggregates
-foreign import countriesImpl :: Fn1 Aggregates Countries
-foreign import countriesListImpl :: ∀ a. EffectFn2 Countries (JSCallback a) Unit
-foreign import providersImpl :: Fn1 Aggregates Providers
-foreign import providersListImpl :: ∀ a. EffectFn2 Providers (JSCallback a) Unit
-foreign import devicesImpl :: Fn1 Aggregates Devices
-foreign import devicesListImpl :: ∀ a. EffectFn2 Devices (JSCallback a) Unit
-foreign import deleteImpl :: ∀ a. EffectFn2 Tags (JSCallback a) Unit
+foreign import listImpl :: ∀ a. EffectFn2 Tags (JSCallback a) Unit
 
-type TagsAttr =
-  { domain :: String
-  , limit :: Int
-  }
-
--- | tags api.
 tags :: Mailgun -> Maybe String -> Tags
-tags mg s = runFn2 tagsImpl mg (toNullable s)
+tags mailgun str = runFn2 tagsImpl mailgun (toNullable str)
 
--- | List all tags.
-list :: ∀ a. Tags -> TagsAttr -> Callback a -> Effect Unit
-list tag cb = runEffectFn2 listImpl tag (handleCallback cb)
-
--- | Gets a specific tag.
-info :: ∀ a. Tags -> Callback a -> Effect Unit
-info t cb = runEffectFn2 infoImpl t (handleCallback cb)
-
--- | tag stats api.
-tagStats :: Tags -> TagStats
-tagStats = runFn1 tagStatsImpl
-
--- | Returns statistics for a given tag.
-tagStatsInfo :: ∀ a. TagStats -> Callback a -> Effect Unit
-tagStatsInfo tag cb = runEffectFn2 tagStatsInfoImpl tag (handleCallback cb)
-
--- | aggregates api.
-aggregates :: TagStats -> Aggregates
-aggregates s = runFn1 aggregatesImpl s
-
--- | countries api.
-countries :: Aggregates -> Countries
-countries a = runFn1 countriesImpl a
-
--- | Returns a list of countries of origin for a given doain for
--- | different event types.
-countriesList :: ∀ a. Countries -> (Callback a) -> Effect Unit
-countriesList c cb = runEffectFn2 countriesListImpl c (handleCallback cb)
-
--- | providers api.
-providers :: Aggregates -> Providers
-providers a = runFn1 providersImpl a
-
--- | Returns a list of email providers for a given domain for different
--- | event types.
-providersList :: ∀ a. Providers -> Callback a -> Effect Unit
-providersList p cb = runEffectFn2 providersListImpl p (handleCallback cb)
-
--- | devices api
-devices :: Aggregates -> Devices
-devices agg = runFn1 devicesImpl agg
-
--- | Returns a list of devices for a given domain that have triggered event types.
-devicesList :: ∀ a. Devices -> Callback a -> Effect Unit
-devicesList dev cb = runEffectFn2 devicesListImpl dev (handleCallback cb)
-
--- | Deletes all counters for particular tag and the tag itself.
-delete :: ∀ a. Tags -> Callback a -> Effect Unit
-delete tag cb = runEffectFn2 deleteImpl tag (handleCallback cb)
+list :: ∀ a. Tags -> Callback a -> Effect Unit
+list tgs cb = runEffectFn2 listImpl tgs (handleCallback cb)
